f4fb4cbc3ee8b051a616fb95bfe3e51aee7d10ea
jjyc <jochung@adobe.com>
2021-01-19 21:08:51 -0500

    update

--- src/mongodb.js
+++ src/mongodb.js
@@ -2,7 +2,9 @@ const MongoClient = require('mongodb').MongoClient;
 
 module.exports = function (app) {
   const connection = app.get('mongodb');
+  console.log('connection', connection)
   const database = connection.substr(connection.lastIndexOf('/') + 1);
+  console.log('database', database)
   const mongoClient = MongoClient.connect(connection, {})
     .then(client => client.db(database));
 
