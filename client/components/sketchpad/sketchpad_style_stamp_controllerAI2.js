6d06aa8da7b6470eb9899eff22546e1a6b075e24
jjyc <jochung@adobe.com>
2021-02-23 14:35:06 -0500

    remove unnecessary

--- client/components/sketchpad/sketchpad_style_stamp_controllerAI2.js
+++ client/components/sketchpad/sketchpad_style_stamp_controllerAI2.js
@@ -382,9 +382,12 @@ class SketchpadStyleStampControllerAI2 extends Component{
         }
         Promise.all(promises).then((value)=>{
             console.log(content, value)
-            Api.app.service('event_logs').create({event: 'transfer_on_sketch', board_id: this.props.mother_this.props.board_this.state.board_id, user_id:this.props.mother_this.props.board_this.state.user_id})
+            
             // analytics.logEvent("transfer_on_sketch", {board_id: this.props.mother_this.props.board_this.state.board_id, user_id:this.props.mother_this.props.board_this.state.user_id})
             Api.app.service('boards').patch(this.props.mother_this.props.board_this.state.board_id, {$set: {updated:'sketchpad_style_apply', content: content, styles: value}})
+            .then(()=>{
+                Api.app.service('event_logs').create({event: 'transfer_on_sketch', board_id: this.props.mother_this.props.board_this.state.board_id, user_id:this.props.mother_this.props.board_this.state.user_id})
+            })
             
         })
     }
