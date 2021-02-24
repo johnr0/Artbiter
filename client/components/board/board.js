089c8171ace3230a4464aacff7e4bd5021f746bd
jjyc <jochung@adobe.com>
2021-02-24 11:07:10 -0500

    layer deletion

--- client/components/board/board.js
+++ client/components/board/board.js
@@ -700,7 +700,7 @@ class Board extends Component{
         console.log(layers)
         set['updated'] = 'sketchpad_remove_a_layer'
         
-        set['layers'] = layers.slice()
+        set['layers'] =  _this.sketchpad.state.layers.slice()
         console.log(layers)
         // layer = JSON.parse(JSON.stringify(layer))
         layer.choosen_by=''
