430098faaa44390c433735262d1c237e2fd594b3
jjyc <jochung@adobe.com>
2021-03-24 09:28:25 -0400

    layer sketchundo not piled

--- src/services/layers/layers.hooks.js
+++ src/services/layers/layers.hooks.js
@@ -5,14 +5,14 @@ const layerPatch = async context =>{
       var sketchundo = context.arguments[1]['$set']['sketchundo']
       context.arguments[1]['$set']['sketchundo'] = undefined
       // console.log(sketchundo)
-      context.app.service('layers').find({query: {_id: context.arguments[0]}})
-      .then((res)=>{
-        sketchundo['layer_id'] = res[0]._id
-        sketchundo['board_id'] = res[0].board_id
-        sketchundo['layer_image'] = res[0].image
-        sketchundo['type']='layer_image'
-        context.app.service('sketchundos').create(sketchundo)
-      })
+      // context.app.service('layers').find({query: {_id: context.arguments[0]}})
+      // .then((res)=>{
+      //   sketchundo['layer_id'] = res[0]._id
+      //   sketchundo['board_id'] = res[0].board_id
+      //   sketchundo['layer_image'] = res[0].image
+      //   sketchundo['type']='layer_image'
+      //   // context.app.service('sketchundos').create(sketchundo)
+      // })
       
     }
   }
